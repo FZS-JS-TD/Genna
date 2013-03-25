a2b8693361d5db35c367aaf0c52c15a48df80e9c
ShuuZero <forrest.z.shooster@gmail.com>
2013-03-25 11:59:36 -0300

    Update Zanaj.cs

--- Genna/Genna/GameObjects/Characters/Players/Zanaj_Akari/Zanaj.cs
+++ Genna/Genna/GameObjects/Characters/Players/Zanaj_Akari/Zanaj.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ using Genna.Items.Weapons;
 
 namespace Genna.GameObjects.Characters.Players.Zanaj_Akari
 {
-    class Zanaj : Player
+    class Zanaj : Players.Player
     {
         protected int speed;
         protected int luck;
